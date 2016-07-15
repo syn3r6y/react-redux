6f413fbf1530374c0cf88ed6ea371ff53388f1a3
Kyle Erickson <kyle@monsterware.ca>
2016-07-15 08:59:23 -0600

    Configure routing and basic style

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-//console.log('hi');
+import 'babel-polyfill';
+import React from 'react';
+import {render} from 'react-dom';
+import { Router, browserHistory } from 'react-dom';
+import routes from './routes';
+import './styles/styles.css'; //Webpack imports css files
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+render(
+    <Router history={browserHistory} routes={routes} />,
+    document.getElementById('app')
+);
