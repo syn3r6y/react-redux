f27a918762adb519a9a7a7407445e562ad2737d9
Kyle Erickson <kyle@monsterware.ca>
2016-07-21 09:05:30 -0600

    Setup to handle text input

--- src/components/course/ManageCoursePage.js
+++ src/components/course/ManageCoursePage.js
@@ -11,27 +11,47 @@ class ManageCoursePage extends React.Component {
 			course: Object.assign({}, props.course),
 			errors: {}
 		};
+
+		this.updateCourseState = this.updateCourseState.bind(this);
     }
+
+    updateCourseState(event){
+		const field = event.target.name;
+		let course = this.state.course;
+		course[field] = event.target.value;
+		return this.setState({course: course});
+	}
 
     render() {
         return (
 				<CourseForm
-					allAuthors={[]}
+					allAuthors={this.props.authors}
+					onChange={this.updateCourseState}
 					course={this.state.course}
 					errors={this.state.errors}
-				/>
+				/>	
 		);
     }
 }
 
-ManageCoursePage.PropTypes = {
-    course: PropTypes.object.isRequired
+ManageCoursePage.propTypes = {
+    course: PropTypes.object.isRequired,
+	authors: PropTypes.array.isRequired
 };
 
 function mapStateToProps(state, ownProps) {
 	let course = {id: '', watchHref: '', title: '', authorId: '', length: '', category: ''};
-    return {
-        course: course
+
+	const authorsFormattedForDropdown = state.authors.map(author => {
+		return {
+			value: author.id,
+			text: author.firstName + ' ' + author.lastName
+		};
+	});
+
+	return {
+        course: course,
+		authors: authorsFormattedForDropdown
     };
 }
 
