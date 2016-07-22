6a4c921578940770b94820bcff8165bc62449061
Kyle Erickson <kyle@monsterware.ca>
2016-07-21 21:04:53 -0600

    Error Handling

--- src/components/course/ManageCoursePage.js
+++ src/components/course/ManageCoursePage.js
@@ -3,6 +3,7 @@ import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import * as courseActions from '../../actions/courseActions';
 import CourseForm from './CourseForm';
+import toastr from 'toastr';
 
 class ManageCoursePage extends React.Component {
     constructor(props, context) {
@@ -36,11 +37,16 @@ class ManageCoursePage extends React.Component {
 		event.preventDefault();
 		this.setState({saving: true});
 		this.props.actions.saveCourse(this.state.course)
-			.then(() => this.redirect());
+			.then(() => this.redirect())
+			.catch(error => {
+				toastr.error(error);
+				this.setState({saving: false});
+			})
 	}
 
 	redirect(){
 		this.setState({saving: false});
+		toastr.success('Course saved.');
 		this.context.router.push('/courses');
 	}
 
